06512adb8cdc5d29c386fc0fe67daffd8ba16ed4
berserkersword <lamjarred063@gmail.com>
2023-05-14 04:17:51 -0700

    main

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { AiOutlineUser } from 'react-icons/ai';
+import { BiBasket } from 'react-icons/bi';
+const Navbar = () => {
+    return (
+
+        <nav className="bg-white border-gray-200 text-[]">
+            <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+                <a href="https://flowbite.com/" className="flex items-center">
+                    <span className="mian-logo self-center whitespace-nowrap text-[2rem]">InterSmart.uz</span>
+                </a>
+
+
+                <div className="flex md:order-1">
+                    <button type="button" data-collapse-toggle="navbar-search" aria-controls="navbar-search" aria-expanded="false" className="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100  rounded-lg text-sm p-2.5 mr-1" >
+                        <svg className="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
+                        <span className="sr-only">Search</span>
+                    </button>
+                    <div className="relative hidden md:block">
+                        <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                            <svg className="w-5 h-5 text-gray-500" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
+                            <span className="sr-only">Search icon</span>
+                        </div>
+                        <input type="text" id="search-navbar" className="block w-[20vw] p-2 pl-10 text-lg text-gray-900 border border-gray-300 rounded-lg bg-gray-50 " placeholder="Search..." />
+
+                    </div>
+                    <button data-collapse-toggle="navbar-search" type="button" className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 " aria-controls="navbar-search" aria-expanded="false">
+                        <span className="sr-only">Open menu</span>
+                        <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
+                    </button>
+                </div>
+
+                <div className="items-center justify-between text-[1.2rem] hidden w-full md:flex md:w-auto md:order-2" id="navbar-search">
+
+                    <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white ">
+                        <li>
+                            <a href="#" className="block py-2 pl-3 pr-4 text-white rounded md:bg-transparent md:text-[#be95c4] md:p-0 " aria-current="page"><AiOutlineUser className='h-[2rem] w-[2rem]' /></a>
+                        </li>
+                        <li>
+                            <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:text-[#be95c4] md:p-0 "><BiBasket className='w-[2rem] h-[2rem]' /></a>
+                        </li>
+                    </ul>
+                </div>
+
+            </div>
+        </nav>
+
+    )
+}
+
+export default Navbar
